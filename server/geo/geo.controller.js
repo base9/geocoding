eff1d2793a2e743d342361099a507d3dc200ecdc
Brian Scoles <brianscoles@gmail.com>
2015-01-08 20:09:49 -0800

    rearrange function order in file, add basic error handling

--- server/geo/geo.controller.js
+++ server/geo/geo.controller.js
@@ -1,5 +1,7 @@
 var Bluebird = require('bluebird');
 var request = Bluebird.promisify(require('request'));
+
+//these variables are used by the throttler
 var queue = [];
 var isAsleep = true;
 var interval = 250;
@@ -10,11 +12,36 @@ var interval = 250;
   //goes out as lat and lng
 function geocodeGoogleAPIRequest(req, clientRes){
   console.log('geocode request received!');
+  if(!req.query.address){
+    clientRes.status(400).end();
+    return;
+  }
   var addressString = req.query.address;
   var formattedAddress = addressString.split(' ').join('+');
   var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='; 
   var reqUrl =  apiUrl + formattedAddress + '&key=' + process.env.GOOGLE_GEOCODING_API_KEY;
+
+  enqueueRequest(sendGeocodeRequest, req, reqUrl, clientRes);
+}
+
+//reverse geocoding request:
+  //comes in as lat and lng
+  //goes out as address object
+function reverseGeocodeGoogleAPIRequest(req, clientRes){
+  console.log('reverse geocode request received!');
+  if(!(req.query.lat && req.query.lng)){
+    clientRes.status(400).end();
+    return;
+  }
+  var formattedCoords = req.query.lat+','+req.query.lng;
+  var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
+  var reqUrl =  apiUrl + formattedCoords + '&key=' + process.env.GOOGLE_GEOCODING_API_KEY;
   
+  enqueueRequest(sendReverseGeocodeRequest, req, reqUrl, clientRes);
+}
+
+
+function sendGeocodeRequest(req, reqUrl, clientRes){
   request(reqUrl)
   .then(function(googleRes){
     if (googleRes.statusCode >= 400) {
@@ -26,61 +53,13 @@ function geocodeGoogleAPIRequest(req, clientRes){
         var lat = json.results[0].geometry.location.lat;
         var lng = json.results[0].geometry.location.lng;
         clientRes.status(200).json([lat,lng]);
+      } else {
+        console.log('Geocoding API did not return a lat/lng');
+        clientRes.status(400).json('Geocoding API did not return a lat/lng');
       }
-      console.log('Geocoding API did not return a lat/lng');
-      clientRes.status(400).json('Geocoding API did not return a lat/lng');
     }
   });
 }
-
-
-
-//reverse geocoding request:
-  //comes in as lat and lng
-  //goes out as address object
-function reverseGeocodeGoogleAPIRequest(req, clientRes){
-  console.log('reverse geocode request received!');
-  var formattedCoords = req.query.lat+','+req.query.lng;
-  var apiUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
-  var reqUrl =  apiUrl + formattedCoords + '&key=' + process.env.GOOGLE_GEOCODING_API_KEY;
-  
-  enqueueRequest(sendReverseGeocodeRequest, req, reqUrl, clientRes);
-
-}
-
-
-function invokeFromQueue(){
-  if(queue.length){
-    isAsleep = false;
-    var next = queue.shift();
-    var fn = next[0];
-    var args = next[1];
-    fn.apply(null,args);
-    setTimeout(invokeFromQueue,interval);
-  } else {
-    isAsleep = true;
-  }
-}
-
-function enqueueRequest(fn){
-  var args = Array.prototype.slice.call(arguments,1);
-  queue.push([fn,args]);
-  if(isAsleep){
-    invokeFromQueue();
-  }
-};
-
-// throttledRequestHandler(hiMary);
-// throttledRequestHandler(hiMary);
-// throttledRequestHandler(hiJoe);
-// throttledRequestHandler(hiJoe);
-// function hiMary(){
-//   console.log('hi, mary.');
-// }
-// function hiJoe(){
-//   console.log('hi, Joe.');
-// }
-
 
 function sendReverseGeocodeRequest(req, reqUrl, clientRes){
   request(reqUrl)
@@ -104,21 +83,29 @@ function sendReverseGeocodeRequest(req, reqUrl, clientRes){
   });
 }
 
+function invokeFromQueue(){
+  if(queue.length){
+    isAsleep = false;
+    var next = queue.shift();
+    var fn = next[0];
+    var args = next[1];
+    fn.apply(null,args);
+    setTimeout(invokeFromQueue,interval);
+  } else {
+    isAsleep = true;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function enqueueRequest(fn){
+  var args = Array.prototype.slice.call(arguments,1);
+  queue.push([fn,args]);
+  if(isAsleep){
+    invokeFromQueue();
+  }
+};
 
 
 module.exports = {
   geocode: geocodeGoogleAPIRequest,
   reverseGeocode: reverseGeocodeGoogleAPIRequest
-}
+};
